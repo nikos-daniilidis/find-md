02a4e6e63d461ab7a8cb5010277f79c616527d60
nikos-daniilidis <nikos.daniilidis@gmail.com>
2014-06-25 19:54:54 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Find-MD
+=======
+
+A repository to help me find the information I need to pick the best doctor 
+that my HMO health insurance plan will allow me to pick. 
+
+The full project involves functions to extract doctor names and addresses 
+from a pdf file that my insurance company allows me to download, scripts to 
+obtain the doctor ratings from several online rating websites, and the tools 
+to analyze the ratings I obtain. These tools can easily be abused, and I will
+not post them here. You can email me if you need to find a doctor. 
+
+What I am sharing here, is the ratings I got for a number of doctors (the 
+doctors') names have been removed, and some neat tools to plot and understand 
+the correlations (if any) for the ratings of the same doctors in different 
+websites. 
---
+++ cs109style.py
@@ -0,0 +1,59 @@
+from __future__ import print_function
+
+from IPython.core.display import HTML
+from matplotlib import rcParams
+
+#colorbrewer2 Dark2 qualitative color table
+dark2_colors = [(0.10588235294117647, 0.6196078431372549, 0.4666666666666667),
+                (0.8509803921568627, 0.37254901960784315, 0.00784313725490196),
+                (0.4588235294117647, 0.4392156862745098, 0.7019607843137254),
+                (0.9058823529411765, 0.1607843137254902, 0.5411764705882353),
+                (0.4, 0.6509803921568628, 0.11764705882352941),
+                (0.9019607843137255, 0.6705882352941176, 0.00784313725490196),
+                (0.6509803921568628, 0.4627450980392157, 0.11372549019607843),
+                (0.4, 0.4, 0.4)]
+
+def customize_mpl():
+    """Tweak matplotlib visual style"""
+    print("Setting custom matplotlib visual style")
+
+    rcParams['figure.figsize'] = (10, 6)
+    rcParams['figure.dpi'] = 150
+    rcParams['axes.color_cycle'] = dark2_colors
+    rcParams['lines.linewidth'] = 2
+    rcParams['axes.grid'] = True
+    rcParams['axes.facecolor'] = '#eeeeee'
+    rcParams['font.size'] = 14
+    rcParams['patch.edgecolor'] = 'none'
+
+
+def customize_css():
+    print("Setting custom CSS for the IPython Notebook")
+    styles = open('custom.css', 'r').read()
+    return HTML(styles)
+
+def remove_border(axes=None, top=False, right=False, left=True, bottom=True):
+    """
+    Minimize chartjunk by stripping out unnecessary plot borders and axis ticks
+    
+    The top/right/left/bottom keywords toggle whether the corresponding plot border is drawn
+    """
+    ax = axes or plt.gca()
+    ax.spines['top'].set_visible(top)
+    ax.spines['right'].set_visible(right)
+    ax.spines['left'].set_visible(left)
+    ax.spines['bottom'].set_visible(bottom)
+    
+    #turn off all ticks
+    ax.yaxis.set_ticks_position('none')
+    ax.xaxis.set_ticks_position('none')
+    
+    #now re-enable visibles
+    if top:
+        ax.xaxis.tick_top()
+    if bottom:
+        ax.xaxis.tick_bottom()
+    if left:
+        ax.yaxis.tick_left()
+    if right:
+        ax.yaxis.tick_right()
